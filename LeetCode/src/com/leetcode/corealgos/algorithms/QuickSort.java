21870e083e69090c6d4f0b3e00c47471d1f46056
Razor Tag <razortag95@gmail.com>
2017-08-27 19:39:58 -0700

    Testing update

--- LeetCode/src/com/leetcode/corealgos/algorithms/QuickSort.java
+++ LeetCode/src/com/leetcode/corealgos/algorithms/QuickSort.java
@@ -7,6 +7,29 @@ package com.leetcode.corealgos.algorithms;
  *
  */
 public class QuickSort {
+	
+	public void sort(Integer[] numArray){
+		int left = -1;
+		int right = numArray.length-1;
+		if(numArray.length > 0){
+			left = 0;
+		}
+		quickSort(numArray, left, right);
+	}
+	
+	public void quickSort(Integer[] numArray, Integer left, Integer right){
+		int mid = partition(numArray, left, right);
+		quickSort(numArray, left, mid-1);
+		quickSort(numArray, mid+1, right);
+	}
+	
+	public int partition(Integer[] numArray, Integer left, Integer right){
+		if(left < 0 || left == right){
+			return -1;
+		}
+		
+		return -1;
+	}
 
 	public static void main(String[] args){
 		
