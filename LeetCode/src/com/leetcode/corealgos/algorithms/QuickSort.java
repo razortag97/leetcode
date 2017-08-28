fd1e322c301cc651590147289d99013f81d00768
Razor Tag <razortag95@gmail.com>
2017-08-27 20:35:44 -0700

    Changes to the QuickSort, presumably final

--- LeetCode/src/com/leetcode/corealgos/algorithms/QuickSort.java
+++ LeetCode/src/com/leetcode/corealgos/algorithms/QuickSort.java
@@ -19,8 +19,10 @@ public class QuickSort {
 	
 	public void quickSort(Integer[] numArray, Integer left, Integer right){
 		int mid = partition(numArray, left, right);
-		quickSort(numArray, left, mid-1);
-		quickSort(numArray, mid+1, right);
+		if(mid > 0){
+			quickSort(numArray, left, mid-1);
+			quickSort(numArray, mid+1, right);
+		}
 	}
 	
 	public int partition(Integer[] numArray, Integer left, Integer right){
@@ -28,7 +30,26 @@ public class QuickSort {
 			return -1;
 		}
 		
-		return -1;
+		Integer pivot = numArray[right];
+		Integer i = left - 1;
+		int j=-1;
+		
+		for(j=left;j<right-1;j++){
+			if(numArray[j].intValue() < pivot.intValue() || numArray[j].intValue() == pivot.intValue()){
+				i++;
+				//Swamp numArray[i] & numArray[j]
+				int temp = numArray[i];
+				numArray[i] = numArray[j];
+				numArray[j] = temp;
+			}
+		}
+		
+		//Exchange numArray[i+1] with numArray[j]
+		int temp = numArray[i+1];
+		numArray[i+1] = numArray[right];
+		numArray[right] = temp;
+		
+		return i+1;
 	}
 
 	public static void main(String[] args){
