963b8a41f766dd7a3975cdaa02738d70653f24d9
Razor Tag <razortag95@gmail.com>
2017-10-15 21:29:07 -0700

    Creating the test case package

---
+++ LeetCode/test/com/leetcode/four/medianoftwosortedarrays/SolutionTest.java
@@ -0,0 +1,20 @@
+package com.leetcode.four.medianoftwosortedarrays;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void testFindMedianSortedArrays() {
+		fail("Not yet implemented");
+	}
+
+}
