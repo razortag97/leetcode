251ce9835bfd44ab4ca9e4696158bcfcd44da627
Razor Tag <razortag95@gmail.com>
2017-10-16 02:40:50 -0700

    LeetCode/src/com/leetcode/three/

---
+++ LeetCode/src/com/leetcode/three/longestsubstring/Solution.java
@@ -0,0 +1,25 @@
+package com.leetcode.three.longestsubstring;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+	public int lengthOfLongestSubstring(String s) {
+		int maxLength = -1;
+		
+		Set<String> testSet = new HashSet<String>();
+		
+		for (String string : s) {
+			
+		}
+		
+		return maxLength;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
