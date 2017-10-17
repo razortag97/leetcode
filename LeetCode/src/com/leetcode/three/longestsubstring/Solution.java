9b641f56a43d2fa03e426831989140325b7205ea
Razor Tag <razortag95@gmail.com>
2017-10-16 22:53:44 -0700

    LeetCode/src/com/leetcode/three/longestsubstring/Solution.java Version: 1.0

--- LeetCode/src/com/leetcode/three/longestsubstring/Solution.java
+++ LeetCode/src/com/leetcode/three/longestsubstring/Solution.java
@@ -6,18 +6,47 @@ import java.util.Set;
 public class Solution {
 
 	public int lengthOfLongestSubstring(String s) {
+		if(s == null || s.isEmpty()){
+			return 0;
+		}
+		
 		int maxLength = -1;
-		
 		Set<String> testSet = new HashSet<String>();
+		int maxCounter =-2125;
+		int counter = 0;
+		int i =0; // String traverser
 		
+		for (i=0;i<s.length();i++){
+			char currentChar = s.charAt(i);
+			String c = new String(new char[]{currentChar});
+			if(testSet.contains((String)c)){
+				if(counter > maxCounter){
+					maxCounter = counter;
+				}
+				testSet.clear();
+				counter=0;
+				
+				//Adding the new char
+				testSet.add(c);
+				counter++;
+			}else{
+				testSet.add(c);
+				counter++;
+			}
+		}
 		
+		if(counter > maxCounter){
+			maxCounter = counter;
+		}
 		
-		return maxLength;
+		return maxCounter;
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Solution s = new Solution();
+		//int length = s.lengthOfLongestSubstring("abcabcbb");
+		int length = s.lengthOfLongestSubstring("c");
+		System.out.println(length);
 	}
 
 }
